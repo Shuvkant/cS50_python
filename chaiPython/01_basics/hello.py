846a46c51e2183278c6bf9ab2c2d45d8c6b31d3c
Shuvkant <weird426344@gmail.com>
2024-11-27 20:53:31 +0545

    startng chai python

---
+++ chaiPython/01_basics/hello.py
@@ -0,0 +1,20 @@
+def chai(n):
+    print(n)
+
+
+chai_one="masala chai"
+chai_two ="mint tea"
+chai_three="flex tea"
+
+"""
+The main function works only when we run the 
+program directly.
+The functions that are not included in main
+will only work when we import this program
+
+"""
+if __name__=="__main__":
+    print("Hello Shuvkant Chaudhary Phanait")
+    chai("Kitkat")
+
+
